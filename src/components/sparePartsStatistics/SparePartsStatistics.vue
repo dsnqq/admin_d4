4ef3dabb54bcfb3176de305c8314bdc2791e86e6
korzhich.d <korzhich.daniil@gmail.com>
2023-09-22 16:15:18 +0300

    sparePartsStatistics +

--- src/components/sparePartsStatistics/SparePartsStatistics.vue
+++ src/components/sparePartsStatistics/SparePartsStatistics.vue
@@ -1,79 +1,103 @@
 <template>
-  <div class="card">
-    <div class="card-body">
-      <div class="row">
-        <table class="table mb-0">
-          <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Дата</th>
-            <th scope="col">Название</th>
-            <th scope="col">Артикул</th>
-            <th scope="col">Статус З/Ч</th>
-            <th scope="col">Кол-во просмотров</th>
-            <th scope="col">Действие</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr
-              v-for="(sparePartsStatisticsItem, i) in SPARE_PARTS_STATISTICS"
-              :key="i"
-          >
-            <th scope="row">
-              {{ sparePartsStatisticsItem.num }}
-            </th>
-            <td>
-              {{ sparePartsStatisticsItem.date }}
-            </td>
-            <td>
-              {{ sparePartsStatisticsItem.name }}
-            </td>
-            <td>
-              {{ sparePartsStatisticsItem.article }}
-            </td>
-            <td>
-              {{ setStatusByApi(sparePartsStatisticsItem.status) }}
-            </td>
-            <td>
-              {{ sparePartsStatisticsItem.views }}
-            </td>
-            <td>
-              <a
-                 target="_blank"
-                 class="p-2 theme-icons"
-                 :href="domain + sparePartsStatisticsItem.linkSite"
-              >
-                <i class="bx bx-show"></i>
-              </a>
-              <a
-                  target="_blank"
-                  class="p-2 theme-icons"
-                 :href="domain + sparePartsStatisticsItem.linkEdit"
-              >
-                <i class="bx bx-pencil"></i>
-              </a>
-            </td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="row">
-        <div class="col-sm-12 col-md-5">
-          <div class="dataTables_info" id="example_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div>
+  <div>
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <table class="table mb-0">
+            <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Дата</th>
+              <th scope="col">Название</th>
+              <th scope="col">Артикул</th>
+              <th scope="col">Статус З/Ч</th>
+              <th scope="col">Кол-во просмотров</th>
+              <th scope="col">Действие</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr
+                v-for="(sparePartsStatisticsItem, i) in SPARE_PARTS_STATISTICS"
+                :key="i"
+            >
+              <th scope="row">
+                {{ sparePartsStatisticsItem.num }}
+              </th>
+              <td>
+                {{ sparePartsStatisticsItem.date }}
+              </td>
+              <td>
+                {{ sparePartsStatisticsItem.name }}
+              </td>
+              <td>
+                {{ sparePartsStatisticsItem.article }}
+              </td>
+              <td>
+                {{ setStatusByApi(sparePartsStatisticsItem.status) }}
+              </td>
+              <td>
+                {{ sparePartsStatisticsItem.views }}
+              </td>
+              <td>
+                <a
+                   target="_blank"
+                   class="p-2 theme-icons"
+                   :href="domain + sparePartsStatisticsItem.linkSite"
+                >
+                  <i class="bx bx-show"></i>
+                </a>
+                <a
+                    target="_blank"
+                    class="p-2 theme-icons"
+                   :href="domain + sparePartsStatisticsItem.linkEdit"
+                >
+                  <i class="bx bx-pencil"></i>
+                </a>
+              </td>
+            </tr>
+            </tbody>
+          </table>
         </div>
-        <div class="col-sm-12 col-md-7">
-          <div class="dataTables_paginate paging_simple_numbers" id="example_paginate">
-            <ul class="pagination">
-              <li class="paginate_button page-item previous disabled" id="example_previous"><a href="#" aria-controls="example" data-dt-idx="0" tabindex="0" class="page-link">Prev</a></li>
-              <li class="paginate_button page-item active"><a href="#" aria-controls="example" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
-              <li class="paginate_button page-item "><a href="#" aria-controls="example" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
-              <li class="paginate_button page-item "><a href="#" aria-controls="example" data-dt-idx="3" tabindex="0" class="page-link">3</a></li>
-              <li class="paginate_button page-item "><a href="#" aria-controls="example" data-dt-idx="4" tabindex="0" class="page-link">4</a></li>
-              <li class="paginate_button page-item "><a href="#" aria-controls="example" data-dt-idx="5" tabindex="0" class="page-link">5</a></li>
-              <li class="paginate_button page-item "><a href="#" aria-controls="example" data-dt-idx="6" tabindex="0" class="page-link">6</a></li>
-              <li class="paginate_button page-item next" id="example_next"><a href="#" aria-controls="example" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li>
-            </ul>
+        <div class="row">
+          <div class="col-sm-12 col-md-5">
+            <div class="dataTables_info" id="example_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div>
           </div>
+          <div class="col-sm-12 col-md-7">
+            <div class="dataTables_paginate paging_simple_numbers" id="example_paginate">
+              <ul class="pagination">
+                <li class="paginate_button page-item previous disabled" id="example_previous"><a href="#" aria-controls="example" data-dt-idx="0" tabindex="0" class="page-link">Prev</a></li>
+                <li class="paginate_button page-item active"><a href="#" aria-controls="example" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
+                <li class="paginate_button page-item "><a href="#" aria-controls="example" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
+                <li class="paginate_button page-item "><a href="#" aria-controls="example" data-dt-idx="3" tabindex="0" class="page-link">3</a></li>
+                <li class="paginate_button page-item "><a href="#" aria-controls="example" data-dt-idx="4" tabindex="0" class="page-link">4</a></li>
+                <li class="paginate_button page-item "><a href="#" aria-controls="example" data-dt-idx="5" tabindex="0" class="page-link">5</a></li>
+                <li class="paginate_button page-item "><a href="#" aria-controls="example" data-dt-idx="6" tabindex="0" class="page-link">6</a></li>
+                <li class="paginate_button page-item next" id="example_next"><a href="#" aria-controls="example" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h6 class="mb-0 text-uppercase">Статистика по датам</h6>
+    <hr>
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <table class="table mb-0">
+            <thead>
+            <tr>
+              <th scope="col">Дата</th>
+              <th scope="col">Количество просмотров</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <th>29.04.2023</th>
+              <th>1085 шт.</th>
+            </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
