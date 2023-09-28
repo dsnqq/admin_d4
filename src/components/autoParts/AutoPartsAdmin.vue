bd84007c6883fb47e790cbf40be291525204d23d
korzhich.d <korzhich.daniil@gmail.com>
2023-09-28 17:55:42 +0300

    автозапчасти пагинация начало

--- src/components/autoParts/AutoPartsAdmin.vue
+++ src/components/autoParts/AutoPartsAdmin.vue
@@ -4,7 +4,7 @@
       <!-- Тут фильтра и прочая история -->
     </div>
     <div class="card-body">
-      <div>
+      <div class="row">
         <div class="table-responsive">
           <table class="table align-middle table-striped table-border-1">
             <thead>
@@ -70,6 +70,15 @@
           </table>
         </div>
       </div>
+      <div class="row">
+        <v-pagination
+            v-model="param.pageNum"
+            :records="autoPartsTotal"
+            :per-page="20"
+            @paginate="setPageByTotal"
+            :options="paginationOptions"
+        ></v-pagination>
+      </div>
     </div>
   </div>
 </template>
@@ -82,25 +91,46 @@
     name: "AutoPartsAdmin",
 
     mounted() {
-      this.GET_AUTO_PARTS_FROM_API();
+      this.GET_AUTO_PARTS_FROM_API(this.param.pageNum);
+      this.GET_AUTO_PARTS_TOTALS();
     },
 
     computed: {
       ...mapGetters('autoParts', [
         'AUTO_PARTS',
+        'AUTO_PARTS_TOTALS'
       ]),
+
+      autoPartsTotal() {
+        return parseInt(this.AUTO_PARTS_TOTALS);
+      }
     },
 
 
     methods: {
       ...mapActions('autoParts', [
         'GET_AUTO_PARTS_FROM_API',
+        'GET_AUTO_PARTS_TOTALS'
       ]),
+
+      setPageByTotal(page) {
+        this.param.pageNum = page;
+        this.GET_AUTO_PARTS_FROM_API(this.param.pageNum);
+      }
     },
 
     data() {
       return {
         domain: DOMAIN,
+        param: {
+          pageNum: 1,
+        },
+        paginationOptions: {
+          chunk: 6,
+          texts: {
+            count: 'Отображается с {from} по {to} (всего {count} шт.)|{count}',
+          }
+        },
       };
     }
 
--- src/vuex/modules/autoParts/autoParts.js
+++ src/vuex/modules/autoParts/autoParts.js
@@ -5,11 +5,15 @@ export default {
     namespaced: true,
 
     state: {
-        autoParts: {}
+        autoParts: {},
+        autoPartsTotals: 0
     },
     getters: {
         AUTO_PARTS(state) {
             return state.autoParts;
+        },
+        AUTO_PARTS_TOTALS(state) {
+            return state.autoPartsTotals;
         },
     },
     actions: {
@@ -30,11 +34,31 @@ export default {
                     return error;
                 });
         },
+
+        GET_AUTO_PARTS_TOTALS({commit}) {
+            return  axios.post(
+                '/index.php?route=api/auto_parts/auto/totals',
+                {
+                    key: KEYS,
+                }
+            )
+                .then((response) => {
+                    commit('SET_AUTO_PARTS_TOTALS_STATE', response.data.autoPartsTotals);
+                    return response.data.autoPartsTotals;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    return error;
+                });
+        },
     },
     mutations: {
         SET_AUTO_PARTS_TO_STATE: (state, autoParts) => {
             state.autoParts = {};
             state.autoParts = autoParts;
+        },
+        SET_AUTO_PARTS_TOTALS_STATE: (state, autoPartsTotals) => {
+            state.autoPartsTotals = autoPartsTotals;
         },
     }
 }
