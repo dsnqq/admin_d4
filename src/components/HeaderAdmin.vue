36d7bfb1f0ed3895193374cd63c83334329c3fa6
korzhich.d <korzhich.daniil@gmail.com>
2023-09-30 15:12:43 +0300

    пару правочек по меню верхнему

--- src/components/HeaderAdmin.vue
+++ src/components/HeaderAdmin.vue
@@ -19,193 +19,43 @@
             </a>
           </li>
           <li class="nav-item dropdown dropdown-user-setting">
-            <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown">
+            <a class="nav-link dropdown-toggle dropdown-toggle-nocaret">
               <div class="user-setting d-flex align-items-center">
                 Даниил Разработчик
               </div>
             </a>
-            <ul class="dropdown-menu dropdown-menu-end">
-              <li>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class="ms-3">
-                      <h6 class="mb-0 dropdown-user-name">Jhon Deo</h6>
-                      <small class="mb-0 dropdown-user-designation text-secondary">HR Manager</small>
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li><hr class="dropdown-divider"></li>
-              <li>
-                <a class="dropdown-item" href="pages-user-profile.html">
-                  <div class="d-flex align-items-center">
-                    <div class=""><i class="bi bi-person-fill"></i></div>
-                    <div class="ms-3"><span>Profile</span></div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class=""><i class="bi bi-gear-fill"></i></div>
-                    <div class="ms-3"><span>Setting</span></div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="index2.html">
-                  <div class="d-flex align-items-center">
-                    <div class=""><i class="bi bi-speedometer"></i></div>
-                    <div class="ms-3"><span>Dashboard</span></div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class=""><i class="bi bi-piggy-bank-fill"></i></div>
-                    <div class="ms-3"><span>Earnings</span></div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class=""><i class="bi bi-cloud-arrow-down-fill"></i></div>
-                    <div class="ms-3"><span>Downloads</span></div>
-                  </div>
-                </a>
-              </li>
-              <li><hr class="dropdown-divider"></li>
-              <li>
-                <a class="dropdown-item" href="authentication-signup-with-header-footer.html">
-                  <div class="d-flex align-items-center">
-                    <div class=""><i class="bi bi-lock-fill"></i></div>
-                    <div class="ms-3"><span>Logout</span></div>
-                  </div>
-                </a>
-              </li>
-            </ul>
           </li>
           <li class="nav-item dropdown dropdown-large">
-            <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown">
+            <a class="nav-link dropdown-toggle dropdown-toggle-nocaret">
               <div class="notifications">
                 <span class="notify-badge">8</span>
                 <i class="bi bi-bell-fill"></i>
               </div>
             </a>
-            <div class="dropdown-menu dropdown-menu-end p-0">
-              <div class="p-2 border-bottom m-2">
-                <h5 class="h5 mb-0">Notifications</h5>
+          </li>
+          <li class="nav-item dropdown dropdown-large">
+            <a class="nav-link dropdown-toggle dropdown-toggle-nocaret">
+              <div class="d-flex align-items-center">
+                <div class="ms-3">
+                  <span>
+                    На главную
+                    <i class="bi bi-house-fill"></i>
+                  </span>
+                </div>
               </div>
-              <div class="header-notifications-list p-2">
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class="notification-box bg-light-primary text-primary"><i class="bi bi-basket2-fill"></i></div>
-                    <div class="ms-3 flex-grow-1">
-                      <h6 class="mb-0 dropdown-msg-user">New Orders <span class="msg-time float-end text-secondary">1 m</span></h6>
-                      <small class="mb-0 dropdown-msg-text text-secondary d-flex align-items-center">You have recived new orders</small>
-                    </div>
-                  </div>
-                </a>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class="notification-box bg-light-purple text-purple"><i class="bi bi-people-fill"></i></div>
-                    <div class="ms-3 flex-grow-1">
-                      <h6 class="mb-0 dropdown-msg-user">New Customers <span class="msg-time float-end text-secondary">7 m</span></h6>
-                      <small class="mb-0 dropdown-msg-text text-secondary d-flex align-items-center">5 new user registered</small>
-                    </div>
-                  </div>
-                </a>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class="notification-box bg-light-success text-success"><i class="bi bi-file-earmark-bar-graph-fill"></i></div>
-                    <div class="ms-3 flex-grow-1">
-                      <h6 class="mb-0 dropdown-msg-user">24 PDF File <span class="msg-time float-end text-secondary">2 h</span></h6>
-                      <small class="mb-0 dropdown-msg-text text-secondary d-flex align-items-center">The pdf files generated</small>
-                    </div>
-                  </div>
-                </a>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class="notification-box bg-light-orange text-orange"><i class="bi bi-collection-play-fill"></i></div>
-                    <div class="ms-3 flex-grow-1">
-                      <h6 class="mb-0 dropdown-msg-user">Time Response  <span class="msg-time float-end text-secondary">3 h</span></h6>
-                      <small class="mb-0 dropdown-msg-text text-secondary d-flex align-items-center">5.1 min avarage time response</small>
-                    </div>
-                  </div>
-                </a>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class="notification-box bg-light-info text-info"><i class="bi bi-cursor-fill"></i></div>
-                    <div class="ms-3 flex-grow-1">
-                      <h6 class="mb-0 dropdown-msg-user">New Product Approved  <span class="msg-time float-end text-secondary">1 d</span></h6>
-                      <small class="mb-0 dropdown-msg-text text-secondary d-flex align-items-center">Your new product has approved</small>
-                    </div>
-                  </div>
-                </a>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class="notification-box bg-light-pink text-pink"><i class="bi bi-gift-fill"></i></div>
-                    <div class="ms-3 flex-grow-1">
-                      <h6 class="mb-0 dropdown-msg-user">New Comments <span class="msg-time float-end text-secondary">2 w</span></h6>
-                      <small class="mb-0 dropdown-msg-text text-secondary d-flex align-items-center">New customer comments recived</small>
-                    </div>
-                  </div>
-                </a>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class="notification-box bg-light-warning text-warning"><i class="bi bi-droplet-fill"></i></div>
-                    <div class="ms-3 flex-grow-1">
-                      <h6 class="mb-0 dropdown-msg-user">New 24 authors<span class="msg-time float-end text-secondary">1 m</span></h6>
-                      <small class="mb-0 dropdown-msg-text text-secondary d-flex align-items-center">24 new authors joined last week</small>
-                    </div>
-                  </div>
-                </a>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class="notification-box bg-light-primary text-primary"><i class="bi bi-mic-fill"></i></div>
-                    <div class="ms-3 flex-grow-1">
-                      <h6 class="mb-0 dropdown-msg-user">Your item is shipped <span class="msg-time float-end text-secondary">7 m</span></h6>
-                      <small class="mb-0 dropdown-msg-text text-secondary d-flex align-items-center">Successfully shipped your item</small>
-                    </div>
-                  </div>
-                </a>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class="notification-box bg-light-success text-success"><i class="bi bi-lightbulb-fill"></i></div>
-                    <div class="ms-3 flex-grow-1">
-                      <h6 class="mb-0 dropdown-msg-user">Defense Alerts <span class="msg-time float-end text-secondary">2 h</span></h6>
-                      <small class="mb-0 dropdown-msg-text text-secondary d-flex align-items-center">45% less alerts last 4 weeks</small>
-                    </div>
-                  </div>
-                </a>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class="notification-box bg-light-info text-info"><i class="bi bi-bookmark-heart-fill"></i></div>
-                    <div class="ms-3 flex-grow-1">
-                      <h6 class="mb-0 dropdown-msg-user">4 New Sign Up <span class="msg-time float-end text-secondary">2 w</span></h6>
-                      <small class="mb-0 dropdown-msg-text text-secondary d-flex align-items-center">New 4 user registartions</small>
-                    </div>
-                  </div>
-                </a>
-                <a class="dropdown-item" href="#">
-                  <div class="d-flex align-items-center">
-                    <div class="notification-box bg-light-bronze text-bronze"><i class="bi bi-briefcase-fill"></i></div>
-                    <div class="ms-3 flex-grow-1">
-                      <h6 class="mb-0 dropdown-msg-user">All Documents Uploaded <span class="msg-time float-end text-secondary">1 mo</span></h6>
-                      <small class="mb-0 dropdown-msg-text text-secondary d-flex align-items-center">Sussessfully uploaded all files</small>
-                    </div>
-                  </div>
-                </a>
+            </a>
+          </li>
+          <li class="nav-item dropdown dropdown-large">
+            <a class="nav-link dropdown-toggle dropdown-toggle-nocaret">
+              <div class="d-flex align-items-center">
+                <div class="ms-3">
+                  <span>
+                    Выйти
+                    <i class="bi bi-lock-fill"></i>
+                  </span>
+                </div>
               </div>
-              <div class="p-2">
-                <div><hr class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">
-                  <div class="text-center">View All Notifications</div>
-                </a>
-              </div>
-            </div>
+            </a>
           </li>
         </ul>
       </div>
