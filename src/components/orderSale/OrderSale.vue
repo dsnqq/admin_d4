62b533b3c0bb55528cfac1e5353b4da59d1870c7
korzhich.d <korzhich.daniil@gmail.com>
2023-11-04 11:42:15 +0300

    фикс + шаблон на странице списка заказов

--- src/components/orderSale/OrderSale.vue
+++ src/components/orderSale/OrderSale.vue
@@ -1,76 +1,78 @@
 <template>
-  <div class="card">
-    <div class="card-header text-center">
+  <LayoutDefault>
+    <template v-slot:header>
       {{DICTIONARY.lastOrderOnTop}}
-    </div>
-    <div class="card-body">
-      <div class="row">
-        <table class="table table-border-1 mb-0 rwd-table middle-responsive">
-          <thead>
-            <tr>
-              <th
-                  v-for="(c, i) in COLUMNS"
-                  :key="i"
-                  scope="col"
-              >
-                {{c.title}}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="(order, i) in ORDER_SALE"
-              :key="i"
-            >
-              <td
-                  :data-th="c.title"
-                  v-for="(c, index) in COLUMNS"
-                  :key="index"
-              >
-                <template v-if="c.type == 'default'">
-                  {{ order[c.name] }}
-                </template>
-                <template v-if="c.type == 'address'">
+    </template>
+    <template v-slot:tableThead>
+      <tr>
+        <th
+            v-for="(c, i) in COLUMNS"
+            :key="i"
+            scope="col"
+        >
+          {{c.title}}
+        </th>
+      </tr>
+    </template>
+    <template v-slot:tableTbody>
+      <tr
+          v-for="(order, i) in ORDER_SALE"
+          :key="i"
+      >
+        <td
+            :data-th="c.title"
+            v-for="(c, index) in COLUMNS"
+            :key="index"
+        >
+          <template v-if="c.type == 'default'">
+            {{ order[c.name] }}
+          </template>
+          <template v-if="c.type == 'address'">
                   <span
                       :title="order[c.name2]"
                       class="cursor-pointer"
                   >
                     {{ order[c.name] }}
                   </span>
-                </template>
-                <template v-else-if="c.type == 'email'">
+          </template>
+          <template v-else-if="c.type == 'email'">
                   <span
                       :title="order[c.name]"
                   >
                     {{ order[c.name] }}
                   </span>
-                </template>
-                <template v-else-if="c.type == 'products'">
-                  <div
-                      v-for="(p, j) in order[c.name]"
-                      :key="j"
-                  >
-                    {{ getInformationAbout(p) }}
-                  </div>
-                </template>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+          </template>
+          <template v-else-if="c.type == 'products'">
+            <div class="products-on-order">
+              <div
+                  v-for="(p, j) in order[c.name]"
+                  :key="j"
+              >
+                {{ getInformationAbout(p) }}
+              </div>
+            </div>
+          </template>
+        </td>
+      </tr>
+    </template>
+    <template v-slot:pagination>
       <Pagination
           :totals="ORDER_TOTALS"
           @setPageByTotal="setPageByTotal"
+          :countChunk="isMobile ? 4 : 5"
+          :class="{'card-body-pagination-mobile' : isMobile}"
       />
-    </div>
-  </div>
+    </template>
+  </LayoutDefault>
 </template>
 
 <script>
+  import LayoutDefault from "@/layouts/LayoutDefault.vue";
   import Pagination from "@/components/UI/BasePagination.vue";
   import {COLUMNS} from "@/components/orderSale/constants/constants";
   import {DICTIONARY} from "@/constants/constants";
   import {mapActions, mapGetters} from "vuex";
+  import {mixins} from "@/mixins/mixins";
 
   export default {
     name: "OrderSale",
@@ -80,6 +82,8 @@
       this.GET_ORDER_TOTALS();
     },
 
+    mixins: [mixins],
+
     computed: {
       ...mapGetters('orderSale', [
         'ORDER_SALE',
@@ -88,6 +92,7 @@
     },
 
     components: {
+      LayoutDefault,
       Pagination
     },
 
@@ -118,5 +123,5 @@
 </script>
 
 <style lang="scss" scoped>
-@import "@/assets/scss/table-adaptive.scss";
+@import "@/components/orderSale/style/order-sale.scss";
 </style>
