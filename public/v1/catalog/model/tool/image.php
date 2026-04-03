2d3abc9526156f4481ade365e4eb2964bc8d9b33
Korzhich.d <korzhich.d@promail366.com>
2026-04-03 17:50:38 +0300

    Revert "images path fixed"
    This reverts commit 88eb7eb7fe2909eecee41ff7ef4475337fa21b6f.

--- public/v1/catalog/model/tool/image.php
+++ public/v1/catalog/model/tool/image.php
@@ -1,46 +1,5 @@
 <?php
 class ModelToolImage extends Model {
-	/**
-	 * Базовый URL витрины для ссылок /image/... (картинки на d4.by, не admin.d4.by).
-	 * 1) HTTP(S)_CATALOG из config.php
-	 * 2) из DIR_IMAGE: .../d4.by/image/ → https://d4.by/
-	 * 3) как OpenCart: config_ssl / config_url
-	 */
-	protected function getImagePublicBase($ssl) {
-		if (defined('HTTPS_CATALOG') && defined('HTTP_CATALOG')) {
-			return $ssl ? rtrim(HTTPS_CATALOG, '/') . '/' : rtrim(HTTP_CATALOG, '/') . '/';
-		}
-		if (defined('HTTPS_CATALOG')) {
-			return rtrim(HTTPS_CATALOG, '/') . '/';
-		}
-		if (defined('HTTP_CATALOG')) {
-			return rtrim(HTTP_CATALOG, '/') . '/';
-		}
-
-		$derived = $this->deriveCatalogBaseUrlFromDirImage();
-		if ($derived !== null) {
-			return $derived;
-		}
-
-		return $ssl ? $this->config->get('config_ssl') : $this->config->get('config_url');
-	}
-
-	/**
-	 * @return string|null https://домен/ или null
-	 */
-	protected function deriveCatalogBaseUrlFromDirImage() {
-		$dir = rtrim(DIR_IMAGE, '/\\');
-		$parent = dirname($dir);
-		$name = basename($parent);
-		if ($name === '' || $name === '.' || strpos($name, '.') === false) {
-			return null;
-		}
-		if (!preg_match('/^[a-z0-9]([a-z0-9.-]*[a-z0-9])?$/i', $name)) {
-			return null;
-		}
-		return 'https://' . $name . '/';
-	}
-
 	public function resize($filename, $width, $height) {
 		if (!is_file(DIR_IMAGE . $filename)) {
 			if (is_file(DIR_IMAGE . 'no_image.jpg')) {
@@ -61,10 +20,7 @@ class ModelToolImage extends Model {
 			list($width_orig, $height_orig, $image_type) = getimagesize(DIR_IMAGE . $image_old);
 
 			if (!in_array($image_type, array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF))) {
-				$parts = explode('/', str_replace('\\', '/', $image_old));
-				$enc = implode('/', array_map('rawurlencode', $parts));
-				$ssl = isset($this->request->server['HTTPS']) && (($this->request->server['HTTPS'] == 'on') || ($this->request->server['HTTPS'] == '1'));
-				return $this->getImagePublicBase($ssl) . 'image/' . $enc;
+				return DIR_IMAGE . $image_old;
 			}
 
 			$path = '';
@@ -91,8 +47,10 @@ class ModelToolImage extends Model {
 		$imagepath_parts = explode('/', $image_new);
 		$new_image = implode('/', array_map('rawurlencode', $imagepath_parts));
 
-		$ssl = isset($this->request->server['HTTPS']) && (($this->request->server['HTTPS'] == 'on') || ($this->request->server['HTTPS'] == '1'));
-
-		return $this->getImagePublicBase($ssl) . 'image/' . $new_image;
+		if (isset($this->request->server['HTTPS']) && (($this->request->server['HTTPS'] == 'on') || ($this->request->server['HTTPS'] == '1'))) {
+			return $this->config->get('config_ssl') . 'image/' . $new_image;
+		} else {
+			return $this->config->get('config_url') . 'image/' . $new_image;
+		}
 	}
 }
