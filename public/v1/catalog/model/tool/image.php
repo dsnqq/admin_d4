8766f16f959747a4bb03d10dd7ee8d30f55e4785
Korzhich.d <korzhich.d@promail366.com>
2026-04-03 15:52:57 +0300

    Revert "test images fixed"
    This reverts commit 590c196d2fa8ec41a0f62c8e96d9a0877136d2bd.

--- public/v1/catalog/model/tool/image.php
+++ public/v1/catalog/model/tool/image.php
@@ -48,9 +48,9 @@ class ModelToolImage extends Model {
 		$new_image = implode('/', array_map('rawurlencode', $imagepath_parts));
 
 		if (isset($this->request->server['HTTPS']) && (($this->request->server['HTTPS'] == 'on') || ($this->request->server['HTTPS'] == '1'))) {
-			return 'https://d4.by/image/' . $new_image;
+			return $this->config->get('config_ssl') . 'image/' . $new_image;
 		} else {
-			return 'http://d4.by/image/' . $new_image;
+			return $this->config->get('config_url') . 'image/' . $new_image;
 		}
 	}
 }
